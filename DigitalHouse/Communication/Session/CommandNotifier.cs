ad35140890423da549afd683324bec5ecdba9bf1
mark <mark@soluto.com>
2015-12-15 15:06:49 +0200

    [Fix] missing file

---
+++ DigitalHouse/Communication/Session/CommandNotifier.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DigitalHouse.BL.CommandExecutors;
+
+namespace DigitalHouse.Communication.Session
+{
+    public class CommandNotifier
+    {
+        private readonly IEnumerable<INewSessionNotifier> mNewSessionNotifier;
+        private readonly ICommandExecutor mCommandExecutor;
+
+        public CommandNotifier(IEnumerable<INewSessionNotifier> notifier, ICommandExecutor commandExecutor)
+        {
+            mCommandExecutor = commandExecutor;
+            mNewSessionNotifier = notifier;
+            RegisterForNewSessions();
+        }
+
+        private void RegisterForNewSessions()
+        {
+            foreach (var sessionNotifier in mNewSessionNotifier)
+            {
+                sessionNotifier.OnNewSession += RegisterSessionForNewMessage;
+            }
+        }
+
+        private void RegisterSessionForNewMessage(IHomeSession homeSession)
+        {
+            homeSession.OnMessageRecieved += mCommandExecutor.ExecuteCommand;
+        }
+    }
+}
